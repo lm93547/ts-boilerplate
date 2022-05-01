5726d2f54e5a3f36007a0db91eb3d0ae3ca90769
Lucas Martin <lucasmartin045@gmail.com>
2022-05-01 13:51:39 +0700

    adding chakra provider and importing new theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
-import './css/global.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from "./styles/theme";
 import About from './pages/about';
 import Home from './pages/home';
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route index element={<Home />} />
-            <Route path='/about' element={<About />} />
-        </Routes>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
